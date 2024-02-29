3f39db2020cf384321b046fe4f0e66e713828d36
ThanhCong19 <congxayda@gmail.com>
2024-02-29 23:36:48 +0700

    fix cmt

--- model.py
+++ model.py
@@ -30,7 +30,7 @@ class FeedForwardBlock(nn.Module):
         self.linear_2 = nn.Linear(d_ff, d_model) # w2 and b2
 
     def forward(self, x):
-        # (batch, seq_len, d_model) --> (batch, seq_len, d_ff) --> (batch, seq_len, d_model)
+        # (batch, seq_len, d_model) --> (batch, seq_len, d_ff) --> (batch, seq_len, d_model) (x.W + b)
         return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
 
 
